87b64532015b3e4fd1ef7b1b5b319a37430d8b60
Jaroslaw Szczegielniak <jszczegielniak@objectivity.co.uk>
2020-05-28 12:12:47 +0200

    Config and launch update

--- launch_all.sh
+++ launch_all.sh
@@ -5,7 +5,7 @@
 # 2019-20 Benjamin Kellenberger
 
 # modules to run
-export AIDE_MODULES=LabelUI
+export AIDE_MODULES=LabelUI,AIController,FileServer
 
 # migrate AIDE (just in case)
 python projectCreation/migrate_aide.py
@@ -15,6 +15,8 @@ python projectCreation/migrate_aide.py
 host=$(python util/configDef.py --section=Server --parameter=host)
 port=$(python util/configDef.py --section=Server --parameter=port)
 
+source ./launch_AIWorker.sh &
 
 # HTTP server
-gunicorn application:app --bind=$host:$port --workers=18
+gunicorn application:app --bind=$host:$port --workers=4
+
